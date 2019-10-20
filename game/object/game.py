159dd09a830334d17075aa503ff0e773fa223869
kijima <norimaking777@gmail.com>
2019-10-20 10:01:30 +0900

    サンプルのmapが完成した。

--- game/object/game.py
+++ game/object/game.py
@@ -5,6 +5,7 @@ KEY_W = 119
 KEY_A = 97
 KEY_S = 115
 KEY_Z = 122
+KEY_D = 100
 
 
 class Map:
@@ -12,7 +13,12 @@ class Map:
         self.width = width
         self.height = height
         # 関数渡しで下記2つのメソッドを勇者インスタンスに渡す
-        self.hero = Hero(3, 3, self.is_movable, self.draw)
+        self.hero = Hero(3, 3, self.is_movable, self.get_message, self.draw)
+        # マップは町人をリストで保持
+        self.townspeople = []
+        self.townspeople.append(Townsman(3, 1, 'K', "I'm King"))
+        self.townspeople.append(Townsman(1, 5, 'S', "I'm Soldier 1"))
+        self.townspeople.append(Townsman(5, 5, 'S', "I'm Soldier 2"))
 
     # ゲームを開始
     def run(self):
@@ -20,6 +26,7 @@ class Map:
 
     # 勇者が座標x, yに動ければTrueを返すメソッド
     def is_movable(self, x, y):
+        # 枠の判定
         if (x < 0):
             return False
         elif (self.width - 1 < x):
@@ -28,13 +35,22 @@ class Map:
             return False
         elif (self.height - 1 < y):
             return False
+
+        # 町人の判定
+        for townsman in self.townspeople:
+            if(x == townsman.x and y == townsman.y):
+                return False
+
         return True
 
     # 画面に現在の状態を描画するメソッド
-    def draw(self):
+    def draw(self, message):
         # 辞書のキーにx,yのタプル、バリューにキャラクターアイコンを登録
         characters = {}
         characters[(self.hero.x, self.hero.y)] = self.hero.icon
+        # キャラクターアイコンの辞書に町人を追加
+        for townsman in self.townspeople:
+            characters[(townsman.x, townsman.y)] = townsman.icon
 
         # 各行をテキストで返すメソッド内の関数
         def get_row(y):
@@ -56,17 +72,40 @@ class Map:
             map_list.append(get_row(y))
         map_list.append('+{}+\n'.format('-' * self.width))
 
+        map_list.append('{}\n'.format('#' * 10))
+        map_list.append(message + '\n')
+        map_list.append('{}\n'.format('#' * 10))
+
         print(''.join(map_list))
+
+    def get_message(self, x, y):
+        for townsman in self.townspeople:
+            if(x == townsman.x and y == townsman.y):
+                return townsman.message
+        return 'no one exists..'
 
 
 class Hero:
     # マップのis_movable と drawメソッドを受け取り登録
-    def __init__(self, x, y, is_movable, draw_map):
+    def __init__(self, x, y, is_movable, get_message ,draw_map):
         self.x = x
         self.y = y
         self.icon = '^'
         self.is_movable = is_movable
+        self.get_message = get_message
         self.draw_map = draw_map
+
+    def talk(self):
+        message = ''
+        if(self.icon == '^'):
+            message = self.get_message(self.x, self.y-1)
+        elif(self.icon == '<'):
+            message = self.get_message(self.x-1, self.y)
+        elif(self.icon == '>'):
+            message = self.get_message(self.x+1, self.y)
+        elif(self.icon == 'V'):
+            message = self.get_message(self.x, self.y+1)
+        return message
 
     def run(self):
         print('----------------------')
@@ -75,6 +114,8 @@ class Hero:
         print('----------------------')
 
         while (True):
+            message = ''
+
             key = ord(getch.getch())
             if (key == KEY_CTRL_C):
                 print('bye!!')
@@ -95,12 +136,22 @@ class Hero:
                 self.icon = 'V'
                 if (self.is_movable(self.x, self.y+1)):
                     self.y += 1
+            elif(key == KEY_D):
+                # 会話をしてメッセージを取得
+                message = self.talk()
             else:
+                print('key input: ' + str(key))
                 continue
             # print('ICON:{}, X:{}, Y:{}'.format(self.icon,self.x, self.y))
 
-            self.draw_map()
+            self.draw_map(message)
 
+class Townsman:
+    def __init__(self, x, y, icon, message):
+        self.x = x
+        self.y = y
+        self.icon = icon
+        self.message = message
 
 dqmap = Map(7, 7)
 dqmap.run()
